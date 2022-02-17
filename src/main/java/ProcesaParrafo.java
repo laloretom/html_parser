d012667534649e2d242a70a07683c2f0cfee673c
Luis Loreto <a216207287@unison.mx>
2022-02-17 11:33:18 -0700

    Lista de StopWords

--- src/main/java/ProcesaParrafo.java
+++ src/main/java/ProcesaParrafo.java
@@ -1,24 +1,45 @@
 import javax.swing.text.MutableAttributeSet;
 import javax.swing.text.html.HTML;
 import javax.swing.text.html.HTMLEditorKit;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashMap;
-import java.util.StringTokenizer;
+import java.util.Set;
 import java.util.TreeSet;
 
 public class ProcesaParrafo extends HTMLEditorKit.ParserCallback {
     private int contador;
     private boolean inParagraph;
+    private Set<String> stopWords = null;
 
     public ProcesaParrafo() {
         contador = 0;
         inParagraph = false;
+        stopWords = this.stopWord();
     }
 
+
+    public Set<String> stopWord(){
+        TreeSet<String> set = new TreeSet<>();
+        try {
+            FileReader fl = new FileReader("stop-word-list.txt");
+            BufferedReader in = new BufferedReader(fl);
+            String linea = null;
+            while ((linea = in.readLine()) != null) {
+                set.add(linea.trim());
+            }
+            in.close();
+        } catch (IOException exception){
+            exception.printStackTrace();
+        }
+        return set;
+    }
 
     @Override
     public void handleText(char[] data, int pos) {
         if (inParagraph) {
-            String texto = new String(data);
+            String texto = new String(data).toLowerCase();
             HashMap<String, Integer> mapa = new HashMap<>();
 
             texto = texto.replaceAll("[\\—\\.\\,\\(\\)]", "");
@@ -29,15 +50,21 @@ public class ProcesaParrafo extends HTMLEditorKit.ParserCallback {
             for (String palabra : palabras) {
                 if (!palabra.matches("\\d*")) {
                     contadorTotal++;
-                    if (mapa.containsKey(palabra)) {
-                        int cuenta = mapa.get(palabra);
-                        mapa.put(palabra, cuenta + 1);
-                    }
-                    else {
-                        mapa.put(palabra, 1);
+
+                    if (!stopWords.contains(palabra)){
+                        if (mapa.containsKey(palabra))
+                        {
+                            int cuenta = mapa.get(palabra.toLowerCase());
+                            mapa.put(palabra, cuenta + 1);
+                        } // fin de if
+                        else
+                        {
+                            mapa.put(palabra, 1);
+                        }
                     }
                 }
             }
+            //System.out.println(mapa);
             System.out.printf("El parafo %d tiene: %7d palabras, %3d Diferentes%n", contador+1, contadorTotal,mapa.size());
         }
     }
